c0306c3efd9fc90b46b03edf235374f6afa3505b
arenas23 <arenas9808@gmail.com>
2024-03-22 09:34:43 -0500

    menu inicial

--- FruitRacing/Assets/Scripts/MenuScripts/MenuManager.cs
+++ FruitRacing/Assets/Scripts/MenuScripts/MenuManager.cs
@@ -5,8 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void StartGame()
+    Canvas canvasMenu1;
+    Canvas canvasMenu2;
+
+    void Start()
+    {
+        Canvas[] canvases = FindObjectsOfType<Canvas>();
+        foreach (Canvas canvas in canvases)
+        {
+            if (canvas.name == "CanvasMenu")
+            {
+                canvasMenu1 = canvas;
+            }
+            else if (canvas.name == "CanvasMenu2")
+            {
+                canvasMenu2 = canvas;
+            }
+        }
+
+        canvasMenu2.gameObject.SetActive(false);
+    }
+
+    public void StartLevel1()
     {
         SceneManager.LoadScene("Level 1 Test");
+    }
+
+    public void StartLevel2()
+    {
+
+    }
+
+    public void StartLevel3()
+    {
+
+    }
+
+    public void ChangeMenu()
+    {
+        canvasMenu1.gameObject.SetActive(false);
+        canvasMenu2.gameObject.SetActive(true);
+    }
+
+    public void BackMenu()
+    {
+        canvasMenu2.gameObject.SetActive(false);
+        canvasMenu1.gameObject.SetActive(true);
     }
 }
